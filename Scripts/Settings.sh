19e33b6d1d07a3a61055c8db73a7d8dd119c02d5
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-09-07 22:31:30 +0800

    Update Settings.sh
    看注释

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -36,3 +36,43 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
 	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
 fi
+
+# 想要剔除的
+echo "CONFIG_PACKAGE_htop=n" >> ./.config
+echo "CONFIG_PACKAGE_iperf3=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-wolplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-design=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-alpha=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-alpha-config=n" >> ./.config
+
+# 可以让FinalShell查看文件列表并且ssh连上不会自动断开
+echo "CONFIG_PACKAGE_openssh-server=y" >> ./.config
+# 解析、查询、操作和格式化 JSON 数据
+echo "CONFIG_PACKAGE_jq=y" >> ./.config
+简单明了的系统资源占用查看工具
+echo "CONFIG_PACKAGE_btop=y" >> ./.config
+# 多网盘存储
+echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+# 强大的工具(需要添加源或git clone)
+echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
+# 网络通信工具
+echo "CONFIG_PACKAGE_curl=y" >> ./.config
+# CPU 性能优化调节设置
+echo "CONFIG_PACKAGE_luci-app-cpufreq=y" >> ./.config
+
+
+# 测试！！！！！！！！！！！！！！！
+echo "CONFIG_PACKAGE_iptables=y" >> ./.config
+echo "CONFIG_PACKAGE_iptables-mod-extra=y" >> ./.config
+echo "CONFIG_PACKAGE_ip6tables=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-ipt-physdev=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-nf-ipvs=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-veth=y" >> ./.config
+echo "CONFIG_PACKAGE_libip4tc2=y" >> ./.config
+echo "CONFIG_PACKAGE_libip6tc2=y" >> ./.config
+echo "CONFIG_PACKAGE_libiptext0=y" >> ./.config
+echo "CONFIG_PACKAGE_libiptext6-0=y" >> ./.config
+echo "CONFIG_PACKAGE_kmod-ipt-fullconenat=y" >> ./.config
