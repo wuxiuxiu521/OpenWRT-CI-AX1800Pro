8d2811cfe35e3d4a52f9cf7715004c034b101c92
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-09-07 22:02:00 +0800

    Update Packages.sh
    调整回官方主题 更新带有clashapi的homeproxy

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -21,23 +21,30 @@ UPDATE_PACKAGE() {
 }
 
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
-UPDATE_PACKAGE "design" "kenzok78/luci-theme-design" "js"
-UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
-UPDATE_PACKAGE "alpha" "derisamedia/luci-theme-alpha" "master"
-UPDATE_PACKAGE "alpha-config" "animegasan/luci-app-alpha-config" "master"
+# 去除argon替换 因为没有GHSer
+# UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
+去除无用主题
+# UPDATE_PACKAGE "design" "kenzok78/luci-theme-design" "js"
+# UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
+# UPDATE_PACKAGE "alpha" "derisamedia/luci-theme-alpha" "master"
+# UPDATE_PACKAGE "alpha-config" "animegasan/luci-app-alpha-config" "master"
 
-UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+# UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
+# 使用不良0的带clashapi的homeproxy 无法显示需要开启无痕
+UPDATE_PACKAGE "homeproxy" "bulianglin/homeproxy" "master"
+
 UPDATE_PACKAGE "mihomo" "morytyann/OpenWrt-mihomo" "main"
 UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
 
-UPDATE_PACKAGE "luci-app-advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
+# 和immortalwrt的argon冲突 故去除
+# UPDATE_PACKAGE "luci-app-advancedplus" "VIKINGYFY/luci-app-advancedplus" "main"
 UPDATE_PACKAGE "luci-app-gecoosac" "lwb1978/openwrt-gecoosac" "main"
-UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
-UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/luci-app-wolplus" "main"
+# 用不上所以不更新了
+# UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+# UPDATE_PACKAGE "luci-app-wolplus" "VIKINGYFY/luci-app-wolplus" "main"
 
 if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
@@ -80,4 +87,5 @@ UPDATE_VERSION() {
 
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 UPDATE_VERSION "sing-box" "true"
-UPDATE_VERSION "tailscale"
+# 用不上所以不更新了
+# UPDATE_VERSION "tailscale"
