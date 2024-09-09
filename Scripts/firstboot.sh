f8a545b74e1ac833995f4803f88276683514b852
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-09-09 18:47:19 +0800

    Update firstboot.sh
    修改第一次启动脚本

--- Scripts/firstboot.sh
+++ Scripts/firstboot.sh
@@ -1,19 +1,27 @@
-# 在OpenWrt根目录下创建files目录
-mkdir -p files/etc/uci-defaults
-
-# 在files目录下创建一个名为99-custom-init的脚本
-cat << "EOF" > files/etc/uci-defaults/99-custom-init
 #!/bin/sh
-# 这里写你想在第一次启动时运行的命令
 
-# echo "Hello, this is a custom command for the first boot." > /etc/firstboot.log
-# 设置webui响应倒计时1s
+# Set Luci apply holdoff to 1
 uci set luci.apply.holdoff='1'
 uci commit luci
 
-# 完成后删除脚本以避免再次执行
-rm -f /etc/uci-defaults/99-custom-init
-EOF
+# Remove ULA prefix and other unwanted settings
+uci del network.globals.ula_prefix
+uci del dhcp.lan.ra_slaac
+uci del dhcp.lan.ra_flags
+uci add_list dhcp.lan.ra_flags='none'
+uci set dhcp.lan.dns_service='0'
+uci del network.globals.ula_prefix
 
-# 确保脚本是可执行的
-chmod +x files/etc/uci-defaults/99-custom-init
+# Configure IPv6 settings
+uci set network.lan.delegate='0'
+uci set network.lan.ip6assign='64'
+uci set network.lan.ip6ifaceid='random'
+
+# Commit changes
+uci commit network
+uci commit dhcp
+
+# Restart services to apply changes
+/etc/init.d/network restart
+/etc/init.d/dnsmasq restart
+/etc/init.d/rpcd restart
