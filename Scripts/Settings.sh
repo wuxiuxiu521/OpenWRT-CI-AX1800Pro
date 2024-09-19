49443b0551a5fe42884c3eafd36ffce78500679d
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-09-19 18:11:39 +0800

    Update Settings.sh
    1.
    .github/workflows/Auto-Clean.yml
    去除每天早上4点自动清理
    
    2.
    .github/workflows/OWRT-ALL.yml
    去除每天早上4点自动清理后自动编译
    
    3.
    .github/workflows/QCA-ALL.yml
    去除每天早上4点自动清理后自动编译
    只保留IPQ60XX带wifi系列
    修改默认主机名 AX1800Pro
    修改默认WIFI名 ImmortalWrt
    修改后台默认地址192.168.6.1
    
    4.
    .github/workflows/WRT-CORE.yml
    	sudo apt-get update -yqq
    	sudo apt-get install -yqq clang-13
    更新clang-13 方便后续编译daed
    	$GITHUB_WORKSPACE/Scripts/feed.sh
    添加kiddin9的源
    
    5.
    Config/IPQ60XX-WIFI-YES.txt
    固件编译只保留AX1800Pro
    
    6.
    Scripts/Packages.sh
    去除主题jerrykuku/luci-theme-argon替换
    去除主题sirpdboy/luci-theme-kucatgen更新
    使用不良0的带clashapi的homeproxy 无法显示需要开启无痕
    去除VIKINGYFY/luci-app-advancedplus更新
    去除luci-app-tailscale更新
    去除VIKINGYFY/luci-app-wolplus更新
    替换golang成最新版的golang
    下载最新版alist
    
    7.
    Scripts/Settings.sh
    去除htop 去除iperf3 去除wolplus 去除tailscale
    去除主题 luci-theme-kucat luci-theme-design
    内置 openssh-sftp-server 可以让FinalShell查看文件列表并且ssh连上不会自动断开
    内置 jq 解析、查询、操作和格式化 JSON 数据
    内置 btop 简单明了的系统资源占用查看工具
    内置 alist luci-app-alist
    内置 lucky 强大的工具(需要添加源或git clone)
    内置 curl 网络通信工具
    内置kiddin9源luci-app-wrtbwmon 图形化流量监控
    内置 kmod-tcp-bbr BBR 拥塞控制算法替换Cubic(单车变摩托)
    内置 luci-app-dockerman docker上游源问题docker只能编译进去

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -33,3 +33,103 @@ if [[ $WRT_TARGET == *"IPQ"* ]]; then
 	echo "CONFIG_ATH11K_MEM_PROFILE_1G=n" >> ./.config
 	echo "CONFIG_ATH11K_MEM_PROFILE_512M=y" >> ./.config
 fi
+
+# 想要剔除的
+echo "CONFIG_PACKAGE_htop=n" >> ./.config
+echo "CONFIG_PACKAGE_iperf3=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-wolplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
+
+# 可以让FinalShell查看文件列表并且ssh连上不会自动断开
+echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
+# 解析、查询、操作和格式化 JSON 数据
+echo "CONFIG_PACKAGE_jq=y" >> ./.config
+# 简单明了的系统资源占用查看工具
+echo "CONFIG_PACKAGE_btop=y" >> ./.config
+# 多网盘存储
+echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+# 强大的工具(需要添加源或git clone)
+echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
+# 网络通信工具
+echo "CONFIG_PACKAGE_curl=y" >> ./.config
+# CPU 性能优化调节设置
+# echo "CONFIG_PACKAGE_luci-app-cpufreq=y" >> ./.config
+# 图形化流量监控
+echo "CONFIG_PACKAGE_luci-app-wrtbwmon=y" >> ./.config
+# bbr加速+turboacc
+# echo "CONFIG_PACKAGE_luci-app-turboacc=y" >> ./.config
+# BBR 拥塞控制算法
+echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
+# docker(只能集成)
+echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
+
+# # luci-app-daed
+# echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
+# # 启用开发选项
+# echo "CONFIG_DEVEL=y" >> ./.config
+# # 启用eBPF支持的主机编译工具链
+# echo "CONFIG_BPF_TOOLCHAIN_HOST=y" >> ./.config
+# # 启用BPF事件
+# echo "CONFIG_KERNEL_BPF_EVENTS=y" >> ./.config
+# # 启用cgroup BPF支持
+# echo "CONFIG_KERNEL_CGROUP_BPF=y" >> ./.config
+# # 启用内核调试信息
+# echo "CONFIG_KERNEL_DEBUG_INFO=y" >> ./.config
+# # 启用BTF格式的内核调试信息
+# echo "CONFIG_KERNEL_DEBUG_INFO_BTF=y" >> ./.config
+# # 启用XDP套接字支持
+# echo "CONFIG_XDP_SOCKETS=y" >> ./.config
+# # 启用XDP套接字诊断
+# echo "CONFIG_XDP_SOCKETS_DIAG=y" >> ./.config
+# # 这些配置项应保持注释状态（即不启用），以避免与 daed 冲突：
+# # 禁用不需要的eBPF编译工具链选项
+# echo "CONFIG_BPF_TOOLCHAIN_NONE=n" >> ./.config
+# # 禁用减少的内核调试信息
+# echo "CONFIG_KERNEL_DEBUG_INFO_REDUCED=n" >> ./.config
+
+# sed -i '/define KernelPackage\/xdp-sockets-diag/,/endef/ { 
+#     s/DEPENDS:=@KERNEL_XDP_SOCKETS//; 
+#     s/KCONFIG:=CONFIG_XDP_SOCKETS_DIAG/KCONFIG:= \\\nCONFIG_XDP_SOCKETS=y \\\nCONFIG_XDP_SOCKETS_DIAG/; 
+#   }' package/kernel/linux/modules/netsupport.mk
+
+# 将 KERNEL_SIZE 的值由 6144kK (6MB)替换为 32768k (32MB) 失败
+# sed -i '/define Device\/jdcloud_ax1800-pro/,/endef/s/KERNEL_SIZE := [0-9]*k/KERNEL_SIZE := 32768k/' target/linux/qualcommax/image/ipq60xx.mk
+# 8M
+# sed -i '/define Device\/jdcloud_ax1800-pro/,/endef/s/KERNEL_SIZE := [0-9]*k/KERNEL_SIZE := 8192k/' target/linux/qualcommax/image/ipq60xx.mk
+
+# XDP 一种高级数据处理技术，旨在提高网络数据包处理的效率和性能。它允许在网络数据包进入内核的更早阶段进行处理，从而减少延迟和提高吞吐量。
+# 基本 XDP 支持
+# echo "CONFIG_XDP=y" >> ./.config
+# # XDP Sockets
+# echo "CONFIG_XDP_SOCKETS=y" >> ./.config
+# # BPF 子系统支持（XDP 是 BPF 的一部分）
+# echo "CONFIG_BPF=y" >> ./.config
+# echo "CONFIG_BPF_SYSCALL=y" >> ./.config
+# # BPF 相关选项（为了全面支持 XDP）
+# echo "CONFIG_NET_CLS_BPF=y" >> ./.config
+# echo "CONFIG_NET_ACT_BPF=y" >> ./.config
+# echo "CONFIG_BPF_JIT=y" >> ./.config
+# echo "CONFIG_HAVE_EBPF_JIT=y" >> ./.config
+# echo "CONFIG_BPF_EVENTS=y" >> ./.config
+# echo "CONFIG_BPF_STREAM_PARSER=y" >> ./.config
+# # eBPF 和 JIT 编译器支持
+# echo "CONFIG_HAVE_BPF_JIT=y" >> ./.config
+# echo "CONFIG_BPF_JIT_ALWAYS_ON=y" >> ./.config
+# # 内核调试信息（可选）: 如果需要调试，可以选择启用内核调试和相关日志支持
+# echo "CONFIG_DEBUG_INFO=n" >> ./.config
+# echo "CONFIG_DEBUG_INFO_REDUCED=n" >> ./.config
+
+# 测试！！！！！！！！！！！！！！！原仓库依旧缺失 iptables ip6tables libip4tc2 libip6tc2 libiptext0 libiptext6-0 修改后依旧缺失 ip6tables libip4tc2 libip6tc2 只能集成docker
+# echo "CONFIG_PACKAGE_iptables=y" >> ./.config
+# echo "CONFIG_PACKAGE_iptables-mod-extra=y" >> ./.config
+# echo "CONFIG_PACKAGE_ip6tables=y" >> ./.config
+# echo "CONFIG_PACKAGE_kmod-ipt-physdev=y" >> ./.config
+# echo "CONFIG_PACKAGE_kmod-nf-ipvs=y" >> ./.config
+# echo "CONFIG_PACKAGE_kmod-veth=y" >> ./.config
+# echo "CONFIG_PACKAGE_libip4tc2=y" >> ./.config
+# echo "CONFIG_PACKAGE_libip6tc2=y" >> ./.config
+# echo "CONFIG_PACKAGE_libiptext0=y" >> ./.config
+# echo "CONFIG_PACKAGE_libiptext6-0=y" >> ./.config
+# echo "CONFIG_PACKAGE_kmod-ipt-fullconenat=y" >> ./.config
