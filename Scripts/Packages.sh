d169376f8ba44192edfd90da91043239dd24d9f3
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-10-13 16:03:09 +0800

    Update Packages.sh
    alist

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -46,6 +46,13 @@ if [[ $WRT_REPO == *"openwrt-6.x"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
 
+# 替换golang成最新版的golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# 下载最新版alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
