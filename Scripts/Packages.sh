fd62229a0a8d4384794b0864deeb7500263ebd30
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2024-11-17 19:46:43 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -45,6 +45,13 @@ if [[ $WRT_REPO != *"immortalwrt"* ]]; then
 	UPDATE_PACKAGE "qmi-wwan" "immortalwrt/wwan-packages" "master" "pkg"
 fi
 
+# 替换golang成最新版的golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+# 下载最新版alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
