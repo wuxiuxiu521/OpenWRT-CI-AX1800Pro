e678f1000f412f70fd1b42e6e3f340fdde24a49e
bailangvvki <962328090@qq.com>
2025-01-29 20:18:20 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -79,11 +79,11 @@ echo "CONFIG_PACKAGE_luci-app-wolplus=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
-echo "CONFIG_PACKAGE_luci-app-mihomo=n" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-mihomo=n" >> ./.config
 # 使用opkg替换apk安装器
-# echo "CONFIG_PACKAGE_opkg=y" >> ./.config
-# echo "CONFIG_OPKG_USE_CURL=y" >> ./.config
-# echo "# CONFIG_USE_APK is not set" >> ./.config
+echo "CONFIG_PACKAGE_opkg=y" >> ./.config
+echo "CONFIG_OPKG_USE_CURL=y" >> ./.config
+echo "# CONFIG_USE_APK is not set" >> ./.config
 # 可以让FinalShell查看文件列表并且ssh连上不会自动断开
 echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
 # 解析、查询、操作和格式化 JSON 数据
@@ -111,7 +111,7 @@ echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
 # docker只能集成
 echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
 # qBittorrent
-#echo "CONFIG_PACKAGE_luci-app-qbittorrent=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-qbittorrent=y" >> ./.config
 # 添加Homebox内网测速
 # echo "CONFIG_PACKAGE_luci-app-homebox=y" >> ./.config
 # V2rayA
@@ -120,12 +120,12 @@ echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
 # echo "CONFIG_PACKAGE_v2ray-geoip=y" >> ./.config
 # echo "CONFIG_PACKAGE_v2ray-geosite=y" >> ./.config
 # NSS的sqm
-#echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-#echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 # NSS MASH
-#echo "CONFIG_ATH11K_NSS_MESH=y" >> ./.config
+echo "CONFIG_ATH11K_NSS_MESH=y" >> ./.config
 # 不知道什么 加上去
-#echo "CONFIG_PACKAGE_MAC80211_NSS_REDIRECT=y" >> ./.config
+# echo "CONFIG_PACKAGE_MAC80211_NSS_REDIRECT=y" >> ./.config
 # istore 编译报错
 echo "CONFIG_PACKAGE_luci-app-istorex=y" >> ./.config
 # QuickStart
@@ -133,6 +133,6 @@ echo "CONFIG_PACKAGE_luci-app-istorex=y" >> ./.config
 # filebrowser-go
 echo "CONFIG_PACKAGE_luci-app-filebrowser-go=y" >> ./.config
 # 图形化web UI luci-app-uhttpd	
-#echo "CONFIG_PACKAGE_luci-app-uhttpd=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-uhttpd=y" >> ./.config
 # 多播
 # echo "CONFIG_PACKAGE_luci-app-syncdial=y" >> ./.config
