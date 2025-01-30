49573c6643da043f106f66bdaeaca46bade89c62
bailangvvki <962328090@qq.com>
2025-01-30 18:31:05 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,7 +49,7 @@ cp -r ../files ./
 # echo "CONFIG_PACKAGE_luci=y" >> ./.config
 # echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 # echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> ./.config
 
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
