fef0105b0210aadccd50b3dc5f9ad9cf390e8dda
bailangvvkg <bailangvvking@outlook.com>
2025-02-18 12:24:37 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -107,4 +107,6 @@ mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubu
 # # luci-app-daed-next
 # git clone https://github.com/sbwml/luci-app-daed-next package/daed-next
 
-git_sparse_clone main https://github.com/kenzok8/small-package luci-app-natter2 natter2 daed-next luci-app-daed-next
+git_sparse_clone main https://github.com/kenzok8/small-package daed-next luci-app-daed-next gost luci-app-gost
+
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages natter2 luci-app-natter2
