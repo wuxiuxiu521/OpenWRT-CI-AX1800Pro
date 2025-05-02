b9b25968e7377a5e231a4a8f3dca922ef06c8b11
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2025-05-03 05:07:13 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -72,8 +72,8 @@ echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
 
-# # 可以让FinalShell查看文件列表并且ssh连上不会自动断开
-# echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
+# 可以让FinalShell查看文件列表并且ssh连上不会自动断开
+echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
 # # 解析、查询、操作和格式化 JSON 数据
 # echo "CONFIG_PACKAGE_jq=y" >> ./.config
 # # 简单明了的系统资源占用查看工具
@@ -82,8 +82,8 @@ echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
 # # echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
 # # 强大的工具(需要添加源或git clone)
 # echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
-# # 网络通信工具
-# echo "CONFIG_PACKAGE_curl=y" >> ./.config
+# 网络通信工具
+echo "CONFIG_PACKAGE_curl=y" >> ./.config
 # # BBR 拥塞控制算法(终端侧) + CAKE 一种现代化的队列管理算法(路由侧)
 # echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
 # echo "CONFIG_DEFAULT_tcp_bbr=y" >> ./.config
