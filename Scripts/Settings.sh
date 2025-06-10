1899e1fb3e88c87388aca557b41cd74f47f41243
bailangvvkg <bailangvvking@outlook.com>
2025-06-11 07:37:30 +0800

    更新 Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -346,7 +346,7 @@ echo "CONFIG_PACKAGE_luci-app-haproxy-tcp=y" >> ./.config
 # Adguardhome去广告
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
 # cloudflre速度筛选器
-echo "CONFIG_PACKAGE_luci-app-cloudflarespeedtest=y" >> ./.config
+# echo "CONFIG_PACKAGE_luci-app-cloudflarespeedtest=y" >> ./.config
 # OpenClash
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
 # nfs-kernel-server共享
