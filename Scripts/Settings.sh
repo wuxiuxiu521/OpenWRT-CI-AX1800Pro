5ce14991d61a7568374820fb6d865a5f876f240c
bailangvvkg <bailangvvking@outlook.com>
2025-06-10 10:28:07 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -244,133 +244,116 @@ fi
 # sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" target/linux/qualcommax/image/ipq60xx.mk
 
 function set_kernel_size() {
-  # 修改 re-ss-01 (亚瑟) re-cs-02 (雅典娜) re-cs-07 (太乙) 等设备的内核大小为 12M
+  #修改jdc ax1800 pro 的内核大小为12M
   image_file='./target/linux/qualcommax/image/ipq60xx.mk'
-  
-  declare -A devices=(
-    [jdcloud_re-ss-01]=6144k
-    [jdcloud_re-cs-02]=6144k
-    [jdcloud_re-cs-07]=6144k
-    [redmi_ax5-jdcloud]=6144k
-    [linksys_mr]=8192k
-  )
-
-  for device in "${!devices[@]}"; do
-    old_size=${devices[$device]}
-    sed -i "/^define Device\/$device/,/^endef/ { /KERNEL_SIZE := $old_size/s//$old_size := 12288k/ }" "$image_file"
-  done
+  sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+  sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+  sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+  sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
+  sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
 }
-# function set_kernel_size() {
-#   #修改jdc ax1800 pro 的内核大小为12M
-#   image_file='./target/linux/qualcommax/image/ipq60xx.mk'
-#   sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#   sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#   sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#   sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#   sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
-# }
 set_kernel_size
 
 # # 想要剔除的
-# # echo "CONFIG_PACKAGE_htop=n" >> ./.config
-# # echo "CONFIG_PACKAGE_iperf3=n" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-wolplus=n" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
-# echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-mihomo=n" >> ./.config
-# # 使用opkg替换apk安装器
-# # echo "CONFIG_PACKAGE_opkg=y" >> ./.config
-# # echo "CONFIG_OPKG_USE_CURL=y" >> ./.config
-# # echo "# CONFIG_USE_APK is not set" >> ./.config
-# # 可以让FinalShell查看文件列表并且ssh连上不会自动断开
-# echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
-# # 解析、查询、操作和格式化 JSON 数据
-# echo "CONFIG_PACKAGE_jq=y" >> ./.config
-# # base64 修改码云上的内容 需要用到
-# echo "CONFIG_PACKAGE_coreutils-base64=y" >> ./.config
-# echo "CONFIG_PACKAGE_coreutils=y" >> ./.config
-# # 简单明了的系统资源占用查看工具
-# echo "CONFIG_PACKAGE_btop=y" >> ./.config
-# # # 多网盘存储
-# # echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
-# # # 强大的工具Lucky大吉(需要添加源或git clone)
-# # echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
-# # # 网络通信工具
-# # echo "CONFIG_PACKAGE_curl=y" >> ./.config
-# # echo "CONFIG_PACKAGE_tcping=y" >> ./.config
-# # # BBR 拥塞控制算法(终端侧)
-# # echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
-# # echo "CONFIG_DEFAULT_tcp_bbr=y" >> ./.config
-# # # 磁盘管理
-# # echo "CONFIG_PACKAGE_luci-app-diskman=y" >> ./.config
-# # echo "CONFIG_PACKAGE_cfdisk=y" >> ./.config
-# # # 其他调整
-# # # 大鹅
-# # echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
-# # # 大鹅-next
-# # # echo "CONFIG_PACKAGE_luci-app-daed-next=y" >> ./.config
-# # # 连上ssh不会断开并且显示文件管理
-# # echo "CONFIG_PACKAGE_opeh-sftp-server"=y
-# # # docker只能集成
-# # echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
-# # # qBittorrent
-# # # echo "CONFIG_PACKAGE_luci-app-qbittorrent=y" >> ./.config
-# # # 添加Homebox内网测速
-# # # echo "CONFIG_PACKAGE_luci-app-homebox=y" >> ./.config
-# # # V2rayA
-# # echo "CONFIG_PACKAGE_luci-app-v2raya=y" >> ./.config
-# # echo "CONFIG_PACKAGE_v2ray-core=y" >> ./.config
-# # echo "CONFIG_PACKAGE_v2ray-geoip=y" >> ./.config
-# # echo "CONFIG_PACKAGE_v2ray-geosite=y" >> ./.config
-# # # NSS的sqm
-# # # echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-# # # echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
-# # # NSS MASH
-# # # echo "CONFIG_ATH11K_NSS_MESH=y" >> ./.config
-# # # 不知道什么 加上去
-# # # echo "CONFIG_PACKAGE_MAC80211_NSS_REDIRECT=y" >> ./.config
-# # # istore 编译报错
-# # # echo "CONFIG_PACKAGE_luci-app-istorex=y" >> ./.config
-# # # QuickStart
-# # # echo "CONFIG_PACKAGE_luci-app-quickstart=y" >> ./.config
-# # # filebrowser-go
-# # # echo "CONFIG_PACKAGE_luci-app-filebrowser-go=y" >> ./.config
-# # # 图形化web UI luci-app-uhttpd	
-# # echo "CONFIG_PACKAGE_luci-app-uhttpd=y" >> ./.config
-# # # 多播
-# # # echo "CONFIG_PACKAGE_luci-app-syncdial=y" >> ./.config
-# # # MosDNS
-# # echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> ./.config
-# # # Natter2 报错
-# # # echo "CONFIG_PACKAGE_luci-app-natter2=y" >> ./.config
-# # # 文件管理器
-# # echo "CONFIG_PACKAGE_luci-app-filemanager=y" >> ./.config
-# # # 不要coremark 避免多线程编译报错
-# # # echo "CONFIG_PACKAGE_coremark=n" >> ./.config
-# # # 基于Golang的多协议转发工具
-# # echo "CONFIG_PACKAGE_luci-app-gost=y" >> ./.config
-# # # Go语言解析
-# # # echo "CONFIG_PACKAGE_golang=y" >> ./.config
-# # # Git
-# # echo "CONFIG_PACKAGE_git-http=y" >> ./.config
-# # # Nginx替换Uhttpd
-# # echo "CONFIG_PACKAGE_nginx-mod-luci=y" >> ./.config
-# # # Nginx的图形化界面
-# # echo "CONFIG_PACKAGE_luci-app-nginx=y" >> ./.config
-# # # HAProxy 比Nginx更强大的反向代理服务器
-# # echo "CONFIG_PACKAGE_luci-app-haproxy-tcp=y" >> ./.config
-# # # Adguardhome去广告
-# # echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
-# # # cloudflre速度筛选器
-# # echo "CONFIG_PACKAGE_luci-app-cloudflarespeedtest=y" >> ./.config
-# # # OpenClash
-# # echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
-# # # nfs-kernel-server共享
-# # echo "CONFIG_PACKAGE_nfs-kernel-server=y" >> ./.config
-# # # Kiddin9 luci-app-nfs
-# # echo "CONFIG_PACKAGE_luci-app-nfs=y" >> ./.config
-# # # zoneinfo-asia tzdata（时区数据库）的一部分，只包含亚洲相关的时区数据 zoneinfo-all全部时区（体积较大，不推荐在嵌入设备）
-# # echo "CONFIG_PACKAGE_zoneinfo-all=y" >> ./.config
-# # # Caddy
-# # echo "CONFIG_PACKAGE_luci-app-caddy=y" >> ./.config
+# echo "CONFIG_PACKAGE_htop=n" >> ./.config
+# echo "CONFIG_PACKAGE_iperf3=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-wolplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-tailscale=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-advancedplus=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-theme-kucat=n" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-mihomo=n" >> ./.config
+# 使用opkg替换apk安装器
+# echo "CONFIG_PACKAGE_opkg=y" >> ./.config
+# echo "CONFIG_OPKG_USE_CURL=y" >> ./.config
+# echo "# CONFIG_USE_APK is not set" >> ./.config
+# 可以让FinalShell查看文件列表并且ssh连上不会自动断开
+echo "CONFIG_PACKAGE_openssh-sftp-server=y" >> ./.config
+# 解析、查询、操作和格式化 JSON 数据
+echo "CONFIG_PACKAGE_jq=y" >> ./.config
+# base64 修改码云上的内容 需要用到
+echo "CONFIG_PACKAGE_coreutils-base64=y" >> ./.config
+echo "CONFIG_PACKAGE_coreutils=y" >> ./.config
+# 简单明了的系统资源占用查看工具
+echo "CONFIG_PACKAGE_btop=y" >> ./.config
+# # 多网盘存储
+# echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+# # 强大的工具Lucky大吉(需要添加源或git clone)
+# echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
+# # 网络通信工具
+# echo "CONFIG_PACKAGE_curl=y" >> ./.config
+# echo "CONFIG_PACKAGE_tcping=y" >> ./.config
+# # BBR 拥塞控制算法(终端侧)
+# echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> ./.config
+# echo "CONFIG_DEFAULT_tcp_bbr=y" >> ./.config
+# # 磁盘管理
+# echo "CONFIG_PACKAGE_luci-app-diskman=y" >> ./.config
+# echo "CONFIG_PACKAGE_cfdisk=y" >> ./.config
+# # 其他调整
+# # 大鹅
+# echo "CONFIG_PACKAGE_luci-app-daed=y" >> ./.config
+# # 大鹅-next
+# # echo "CONFIG_PACKAGE_luci-app-daed-next=y" >> ./.config
+# # 连上ssh不会断开并且显示文件管理
+# echo "CONFIG_PACKAGE_opeh-sftp-server"=y
+# # docker只能集成
+# echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
+# # qBittorrent
+# # echo "CONFIG_PACKAGE_luci-app-qbittorrent=y" >> ./.config
+# # 添加Homebox内网测速
+# # echo "CONFIG_PACKAGE_luci-app-homebox=y" >> ./.config
+# # V2rayA
+# echo "CONFIG_PACKAGE_luci-app-v2raya=y" >> ./.config
+# echo "CONFIG_PACKAGE_v2ray-core=y" >> ./.config
+# echo "CONFIG_PACKAGE_v2ray-geoip=y" >> ./.config
+# echo "CONFIG_PACKAGE_v2ray-geosite=y" >> ./.config
+# # NSS的sqm
+# # echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+# # echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
+# # NSS MASH
+# # echo "CONFIG_ATH11K_NSS_MESH=y" >> ./.config
+# # 不知道什么 加上去
+# # echo "CONFIG_PACKAGE_MAC80211_NSS_REDIRECT=y" >> ./.config
+# # istore 编译报错
+# # echo "CONFIG_PACKAGE_luci-app-istorex=y" >> ./.config
+# # QuickStart
+# # echo "CONFIG_PACKAGE_luci-app-quickstart=y" >> ./.config
+# # filebrowser-go
+# # echo "CONFIG_PACKAGE_luci-app-filebrowser-go=y" >> ./.config
+# # 图形化web UI luci-app-uhttpd	
+# echo "CONFIG_PACKAGE_luci-app-uhttpd=y" >> ./.config
+# # 多播
+# # echo "CONFIG_PACKAGE_luci-app-syncdial=y" >> ./.config
+# # MosDNS
+# echo "CONFIG_PACKAGE_luci-app-mosdns=y" >> ./.config
+# # Natter2 报错
+# # echo "CONFIG_PACKAGE_luci-app-natter2=y" >> ./.config
+# # 文件管理器
+# echo "CONFIG_PACKAGE_luci-app-filemanager=y" >> ./.config
+# # 不要coremark 避免多线程编译报错
+# # echo "CONFIG_PACKAGE_coremark=n" >> ./.config
+# # 基于Golang的多协议转发工具
+# echo "CONFIG_PACKAGE_luci-app-gost=y" >> ./.config
+# # Go语言解析
+# # echo "CONFIG_PACKAGE_golang=y" >> ./.config
+# # Git
+# echo "CONFIG_PACKAGE_git-http=y" >> ./.config
+# # Nginx替换Uhttpd
+# echo "CONFIG_PACKAGE_nginx-mod-luci=y" >> ./.config
+# # Nginx的图形化界面
+# echo "CONFIG_PACKAGE_luci-app-nginx=y" >> ./.config
+# # HAProxy 比Nginx更强大的反向代理服务器
+# echo "CONFIG_PACKAGE_luci-app-haproxy-tcp=y" >> ./.config
+# # Adguardhome去广告
+# echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
+# # cloudflre速度筛选器
+# echo "CONFIG_PACKAGE_luci-app-cloudflarespeedtest=y" >> ./.config
+# # OpenClash
+# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> ./.config
+# # nfs-kernel-server共享
+# echo "CONFIG_PACKAGE_nfs-kernel-server=y" >> ./.config
+# # Kiddin9 luci-app-nfs
+# echo "CONFIG_PACKAGE_luci-app-nfs=y" >> ./.config
+# # zoneinfo-asia tzdata（时区数据库）的一部分，只包含亚洲相关的时区数据 zoneinfo-all全部时区（体积较大，不推荐在嵌入设备）
+# echo "CONFIG_PACKAGE_zoneinfo-all=y" >> ./.config
+# # Caddy
+# echo "CONFIG_PACKAGE_luci-app-caddy=y" >> ./.config
