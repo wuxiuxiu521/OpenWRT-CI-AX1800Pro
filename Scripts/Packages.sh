521672d7b95b485c9aa92a29f06ec783ed0c66ec
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2025-06-23 21:32:29 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -148,3 +148,7 @@ git_sparse_clone main https://github.com/kiddin9/kwrt-packages natter2 luci-app-
 wget "https://alist4.lovelyy.eu.org/d/CloudFlareR2/immortalwrt/nginx/ngnx.conf?sign=FN_uiyymuja-Aj1z4I4Pevn3arIZXBdslq8Zjd_akdo=:0" -O ../feeds/packages/net/nginx-util/files/nginx.config
 # echo 检测一下nginx的配置文件
 # cat ../feeds/packages/net/nginx-util/files/nginx.config
+
+sed -i 's/^large_client_header_buffers .*/large_client_header_buffers 8 32k;/' ../feeds/packages/net/nginx-util/files/uci.conf.template
+检测一下nginx包头是否由2个K改成8个32K
+cat ../feeds/packages/net/nginx-util/files/uci.conf.template
