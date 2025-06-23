068e9039ce26336bcd515567f0582a7895d6af55
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2025-06-23 19:31:05 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -84,7 +84,7 @@ echo "CONFIG_PACKAGE_jq=y" >> ./.config
 echo "CONFIG_PACKAGE_btop=y" >> ./.config
 # 多网盘存储
 # echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-openlist=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-openlist=y" >> ./.config
 # 强大的工具(需要添加源或git clone)
 echo "CONFIG_PACKAGE_luci-app-lucky=y" >> ./.config
 # 网络通信工具
