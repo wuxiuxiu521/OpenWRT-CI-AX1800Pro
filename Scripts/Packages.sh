ec962a66b974049eb5acdfa33fdff221c3904904
wuxiuxiu521 <wuxiuxiumama@gmail.com>
2026-01-21 16:31:12 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -123,7 +123,7 @@ UPDATE_VERSION "sing-box"
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
 	branch="$1" repourl="$2" && shift 2
-	git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+	git clone --recursive --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
 	repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
 	cd $repodir && git sparse-checkout set $@
 	mv -f $@ ../package
